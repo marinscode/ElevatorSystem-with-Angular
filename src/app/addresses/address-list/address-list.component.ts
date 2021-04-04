0ba2db52c00d0622a37cc98c7807be5c4386823b
Marin Totev <marin.totev@gmail.com>
2021-04-04 18:15:34 +0100

    adding filter to the address table

--- src/app/addresses/address-list/address-list.component.ts
+++ src/app/addresses/address-list/address-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 
 import { AddressModel } from '../address.model';
@@ -11,9 +12,10 @@ import { AddressesServices } from '../addresses.service';
 })
 export class AddressListComponent implements OnInit {
   addresses: AddressModel[] = [];
-  displayedColumns: string[] = ['id', 'regnum', 'city', 'address', 'floors', 'elevators'];
+  displayedColumns: string[] = ['id', 'regnum', 'city', 'address', 'floors', 'elevators', 'typeElevator', 'maxWeight', 'velocity', 'gfloor', 'basement' ];
   isLoading = false;
   private addressesSub: Subscription;
+  dataSource = new MatTableDataSource(this.addresses);
 
   constructor(public addressesService: AddressesServices) { }
 
@@ -23,7 +25,14 @@ export class AddressListComponent implements OnInit {
     this.addressesSub = this.addressesService.getAddressUpdateListener().subscribe(data => {
       this.addresses = data.addresses;
       this.isLoading = false;
+      this.dataSource = new MatTableDataSource(this.addresses);
+      console.log(this.addresses);
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -42,7 +43,8 @@ import { ElevatorCreateComponent } from './elevator/elevator-create/elevator-cre
     MatSelectModule,
     MatCheckboxModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
